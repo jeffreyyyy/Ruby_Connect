baaaf70d98edf67ef087a86fcf8c1e3b0ff6cc07
Jeffrey Jurgajtis <jeffreyj789@hotmail.com>
2012-01-01 16:55:52 -0600

    created questions resource

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -5,4 +5,24 @@ class QuestionsController < ApplicationController
     @questions = Question.all
   end
   
+  def new
+    @title = "Post Question | Ruby.Connect"
+    @question = Question.new
+  end
+  
+  def show
+    @title = "Ruby.Connect"
+    @question = Question.find(params[:id])
+  end
+  
+  def create
+    @question = Question.new(params[:question])
+    if @question.save
+      redirect_to @question
+    else
+      @title = "Post Question | Ruby.Connect"
+      render 'new'
+    end
+  end
+
 end
